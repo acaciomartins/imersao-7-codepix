603b0e289fe8ce91921c223d21b2425c971c62d3
acaciomartins <acacio.am@gmail.com>
2022-04-05 12:48:17 -0300

    aula 04/04 - Full Cycle, estudo de caso e microsserviços

---
+++ codepix/domain/model/bank.go
@@ -0,0 +1,41 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func (bank *Bank) isValid() error {
+	_, err := govalidator.ValidateStruct(bank)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type Bank struct {
+	Base     `valid:"required"`
+	Code     string     `json:"code" valid: "notnull"`
+	Name     string     `json:"name" valid: "notnull"`
+	Accounts []*Account `valid: "-"`
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreateAt = time.Now()
+
+	err := bank.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &bank, nil
+}
