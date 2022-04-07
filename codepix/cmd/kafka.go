3745fb20fbc7602f7ec299eb7a6da685c65105b5
acaciomartins <acacio.am@gmail.com>
2022-04-07 13:21:41 -0300

    aula 06/04 - Full Cycle - Apache Kafka

--- codepix/cmd/kafka.go
+++ codepix/cmd/kafka.go
@@ -33,8 +33,12 @@ var kafkaCmd = &cobra.Command{
 		database := db.ConnectDB(os.Getenv("env"))
 		producer := kafka.NewKafkaProducer()
 
-		//kafka.Publish("Ola Cosumer", "teste", producer, deliveryChan)
+		kafka.Publish("ola Kafka", "teste", producer, deliveryChan)
 		go kafka.DeliveryReport(deliveryChan)
+		// producer := kafka.NewKafkaProducer()
+
+		//kafka.Publish("Ola Cosumer", "teste", producer, deliveryChan)
+		//go kafka.DeliveryReport(deliveryChan)
 
 		kafkaProcessor := kafka.NewKafkaProcessor(database, producer, deliveryChan)
 		kafkaProcessor.Consume()
